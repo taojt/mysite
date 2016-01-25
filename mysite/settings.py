3d3976d3367318203dbb54ed120468923ce0cfe1
jt1310167566@hotmail.com <jt1310167566@hotmail.com>
2016-01-25 22:31:29 +0800

    add models

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'polls',
 )
 
 ROOT_URLCONF = 'mysite.urls'
