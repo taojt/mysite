23f153a79ab7116c304f56d6ee9abc2e24c90048
jt1310167566@hotmail.com <jt1310167566@hotmail.com>
2016-01-25 23:45:02 +0800

    register Choice by second methord

--- polls/admin.py
+++ polls/admin.py
@@ -6,13 +6,30 @@ from .models import Question, Choice
 # class QuestionAdmin(admin.ModelAdmin):
 # 	fields = ["pub_date", "question_text"]
 
-# 字段集
+# # 字段集
+# class QuestionAdmin(admin.ModelAdmin):
+# 	fieldsets = [
+# 		(None, {"fields": ["question_text"]}),
+# 		("Date Information", {"fields": ["pub_date"], "classes":["collapse"]}),
+# 	]
+
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None, {"fields": ["question_text"]}),
 		("Date Information", {"fields": ["pub_date"], "classes":["collapse"]}),
 	]
+	inlines = [ChoiceInline]
+
+	list_display = ("question_text", "pub_date", "was_published_recently")
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
 
-admin.site.register(Choice)
+# admin.site.register(Choice)
