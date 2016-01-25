da12f2b47a7728f508043dd148ddcb1916712426
jt1310167566@hotmail.com <jt1310167566@hotmail.com>
2016-01-26 00:23:38 +0800

    use templates to render a view

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r"^$", views.index(), name="index"),
+	url(r"^$", views.index, name="index"),
+	url(r"^(?P<question_id>\d+)/$", views.detail, name="detail"),
+	url(r"^(?P<question_id>\d+)/results/$", views.results, name="results"),
+	url(r"^(?P<question_id>\d+)/votes/$", views.vote, name="vote")
+
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,24 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader, RequestContext
+from .models import Question
 
 
 # Create your views here.
 
+# def index(request):
+# 	return HttpResponse("Hello, Django. You're at the polls index.")
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([p.question_text for p in latest_question_list])
+#     return HttpResponse(output)
+
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = RequestContext(request, {"latest_question_list": latest_question_list, })
+	return HttpResponse(template.render(context))
 
 
 def detail(request, question_id):
