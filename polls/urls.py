0db59f5a14e85e912fe31523ea6bca398bfc8dbd
jt1310167566@hotmail.com <jt1310167566@hotmail.com>
2016-01-27 00:01:19 +0800

    通用视图设置

--- polls/urls.py
+++ polls/urls.py
@@ -5,10 +5,18 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r"^$", views.index, name="index"),
-	url(r"^(?P<question_id>\d+)/$", views.detail, name="detail"),
-	url(r"^(?P<question_id>\d+)/results/$", views.results, name="results"),
-	url(r"^(?P<question_id>\d+)/votes/$", views.vote, name="vote")
+	# url(r"^$", views.index, name="index"),
+	# url(r"^(?P<question_id>\d+)/$", views.detail, name="detail"),
+	# url(r"^(?P<question_id>\d+)/results/$", views.results, name="results"),
+	# url(r"^(?P<question_id>\d+)/votes/$", views.vote, name="vote")
+
+	# 改良URLconf
+	url(r'^$', views.IndexView.as_view(), name='index'),
+	url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+	url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+	url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+
+
 
 
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,7 @@ from .models import Question, Choice
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
-from django.http import Http404
+from django.views import generic
 
 
 # Create your views here.
@@ -72,4 +72,26 @@ def vote(request, question_id):
 		selected_choice.votes += 1
 		selected_choice.save()
 		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
-		# return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+	# return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+
+	def get_queryset(self):
+		"""
+		Return the last five published question
+		:return:
+		"""
+		return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
